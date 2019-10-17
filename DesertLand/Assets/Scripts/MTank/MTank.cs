d076e6179cdb7a462502d30155e49cf27ab2e6df
JeanCho <nicetoto97@naver.com>
2019-10-17 18:02:08 +0900

    Finished Cam and Animation Tutorial

---
+++ DesertLand/Assets/Scripts/MTank/MTank.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MTank : MonoBehaviour
+{
+
+
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = this.transform.forward * speed * Time.deltaTime;
+
+        this.transform.position += movement;
+    }
+}
